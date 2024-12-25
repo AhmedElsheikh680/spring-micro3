4e6d23522e86d0101a23d1fde409143c21d86023
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2024-12-26 00:26:25 +0200

    register microservices on eureka server #1

--- Team/src/main/java/com/spring/team/TeamApplication.java
+++ Team/src/main/java/com/spring/team/TeamApplication.java
@@ -2,12 +2,14 @@ package com.spring.team;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients("com.spring.team")
+@EnableDiscoveryClient
 public class TeamApplication {
 
     public static void main(String[] args) {
