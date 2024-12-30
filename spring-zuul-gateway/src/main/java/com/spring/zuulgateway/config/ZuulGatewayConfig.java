b6af933abc14629499ad131e9e916e590a8e541a
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2024-12-30 10:40:08 +0200

    zuul gateway #2

---
+++ spring-zuul-gateway/src/main/java/com/spring/zuulgateway/config/ZuulGatewayConfig.java
@@ -0,0 +1,34 @@
+package com.spring.zuulgateway.config;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.protocol.RequestContent;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class ZuulGatewayConfig extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";  //pre, post, error
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        HttpServletRequest httpServletRequest=  RequestContext.getCurrentContext().getRequest();
+        System.out.println(">>>>>>>>>>>>Request From "+ httpServletRequest.getRequestURI());
+        return null;
+    }
+}
--- spring-zuul-gateway/src/test/java/com/spring/zuulgateway/SpringZuulGatewayApplicationTests.java
+++ spring-zuul-gateway/src/test/java/com/spring/zuulgateway/SpringZuulGatewayApplicationTests.java
@@ -3,7 +3,7 @@ package com.spring.zuulgateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SpringZuulGatewayApplicationTests {
 
     @Test
