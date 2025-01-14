6c406fc14ce66d051539b2e863e746991df2518b
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2025-01-14 21:58:47 +0200

    V2: using spring.config.import=optional:configserver=

--- players-statistics/src/main/java/com/spring/players/PlayersStatisticsApplication.java
+++ players-statistics/src/main/java/com/spring/players/PlayersStatisticsApplication.java
@@ -2,8 +2,11 @@ package com.spring.players;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
+//@EnableHystrix
+//@EnableRetry
 public class PlayersStatisticsApplication {
 
     public static void main(String[] args) {
--- players-statistics/src/main/java/com/spring/players/controller/PlayersStatisticsController.java
+++ players-statistics/src/main/java/com/spring/players/controller/PlayersStatisticsController.java
@@ -2,6 +2,7 @@ package com.spring.players.controller;
 
 import com.spring.players.config.PlayersStatisticsConfig;
 import com.spring.players.model.PlayersStatistics;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +21,26 @@ public class PlayersStatisticsController {
     @GetMapping("/statistics")
     public ResponseEntity<?> getPlayersStatistics() {
         return ResponseEntity.ok(new PlayersStatistics(config.getNumberOfTeams(), config.getNumberOfPlayers(), config.getCountry()));
+    }
+
+//    @GetMapping("/salary")
+////    @HystrixCommand(fallbackMethod = "callBackk")
+//    @Retry(name = "getSalary", fallbackMethod = "callBack")
+//    public String getSalary() {
+//        throw new NullPointerException("Error");
+//    }
+//
+//    public String callBackk() {
+//        return "Service Unavailable !!!!!!!!!!!!!!";
+//    }
+
+    @GetMapping("/salary")
+    @Retry(name = "getSalary", fallbackMethod = "callBackk")
+    public Integer getSalary() {
+        return 1;
+    }
+
+    public Integer callBackk(Exception e) {
+        return 50000;
     }
 }
